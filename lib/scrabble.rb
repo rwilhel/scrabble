6b00262cad6a40e963426e4842c1abdee7fa9591
rwilhel <rwilhel@gmail.com>
2017-04-07 14:05:05 -0600

    Simplification of exising code in scrabble.rb

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,11 +1,11 @@
 class Scrabble
-  attr_reader :word, :extracted_values
 
-  def initialize
-    @word = word
-    @char_set = []
-    @char_index = {}
-    @values = {
+  def char_set(word)
+    @char_set = word.upcase.scan(/./)
+  end
+
+  def point_values
+    {
       "A"=>1, "B"=>3, "C"=>3, "D"=>2,
       "E"=>1, "F"=>4, "G"=>2, "H"=>4,
       "I"=>1, "J"=>8, "K"=>5, "L"=>1,
@@ -14,30 +14,39 @@ class Scrabble
       "U"=>1, "V"=>4, "W"=>4, "X"=>8,
       "Y"=>4, "Z"=>10
     }
-    @final_score = []
   end
 
-  def char_set(word)
-    @char_set = word.upcase.scan(/./)
-  end
-
-  def char_index(char_set)
-    @word_set_2 = Hash[char_set.map {|char| [char, 0]}]
-  end
-
-  def add_up(word_set_2, values)
-    checker = word_set_2.merge(values){ |x, word_value, key_value| word_value + key_value }
-  end
-
-  def extract_new_values(char_set, checker)
-    @extracted_values = checker.reject { |x| !char_set.include?(x.to_s)}
-  end
-
-  def score(user_input)
-    real_key = user_input.upcase @values.values_at(real_key).inject(0, :+)
-  end
-
-  def total_score(extracted_values)
-    extracted_values.values.inject(0) { |x, y| x + y }
-  end
+    def score(word)
+      char_set = word.upcase.scan(/./)
+      char_set.map { |char| point_values[char] }.compact.inject(:+)
+    end
 end
+
+
+# class Scrabble
+#   attr_reader :word, :extracted_values
+#
+#   def initialize
+#     @word = word
+#     @char_set = []
+#     @char_index = {}
+#     @final_score = []
+#   end
+
+
+  #
+  # def char_index(char_set)
+  #   @word_set_2 = Hash[char_set.map {|char| [char, 0]}]
+  # end
+  #
+  # def add_up(word_set_2, values)
+  #   checker = word_set_2.merge(values){ |x, word_value, key_value| word_value + key_value }
+  # end
+  #
+  # def extract_new_values(char_set, checker)
+  #   @extracted_values = checker.reject { |x| !char_set.include?(x.to_s)}
+  # end
+
+  # def score(user_input)
+  #   real_key = user_input.upcase @values.values_at(real_key).inject(0, :+)
+  # end
